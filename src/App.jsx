029a73eac47be29b1aa61647132960e03b02c877
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2023-02-14 01:30:52 +0500

    title changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styles from "./style";
 import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
-
+// yt video and  chose video ko dekh kr update kro
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
