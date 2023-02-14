f6185522628af851ca553b907e4974c5ad61fb02
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2023-02-14 20:27:03 +0500

    header logo fixed

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,7 +4,7 @@ import binance from "./binance.png";
 import card from "./card.png";
 import coinbase from "./coinbase.png";
 import dropbox from "./dropbox.png";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import quotes from "./quotes.svg";
 import robot from "./robot.png";
 import send from "./Send.svg";
@@ -23,6 +23,9 @@ import twitter from "./twitter.svg";
 import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
+import logo from "./xsecure_logo.png";
+// import xsecure_logo2 from "./xsecure_logo2.png";
+
 
 export {
   airbnb,
